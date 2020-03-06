1ceb2c907d554f671817c46df0cb978e7db6633f
Paolo Vega <pao02esc@gmail.com>
2020-03-05 23:15:07 -0600

    Comments in code, debug and optimization in scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,9 @@
 # ===============================================
 # Paolo Vega
-# 03/02/2020
-# Version 1.0
-#   Version 1.1   03/03/2020
+# Bootcamp Data Analytics
+# Version   1.0.0 03/02/2020
+#           1.0.1 03/04/2020
+#           1.0.2 03/05/2020
 # File to run the PyBank analysis of the financial records of our company
 # ===============================================
 
@@ -12,49 +13,63 @@ import csv
 import datetime
 import statistics
 
-folder =  "C:\\Users\\Paolo\\Bootcamp\\Python-Challenge\\Resources"
+# Define the folder and file name location
+folder =  "../Resources"
 csvName = "budget_data.csv"
 csvpath = os.path.join(folder,csvName)
-# Lists to store key and values
+
+# Lists that will store the months, profits and the highest increase
 months = []
 profits = []
 incprofits = []
 
+# initialize variables
 totalValue = 0
 noMonths = 0
 avgChange = 0
 
+# Descriptions for the greaates increase and greatest decrease
 greatestIncrease = 0
 greatestIncMonth = ""
 greatestDecrease = 0
 greatestDecMonth = ""
 
-
+# Open and read the csv file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    header = next(csvreader)   
-    
+    # Store the header to avoid column names
+    header = next(csvreader)
+
+    # Go through each line to create two lists for holding months and their profit
     for row in csvreader:
         months.append(row[0])
         profits.append(float((row[1])))
 
-#initialize variables
-i = 1
-while i < (len(profits)):
+# Calculate the differences each month with respect the previous one to 
+# identify the profit increases
+# iterate from the second row to the last one
+for i in range(1,(len(profits))):
+    # Calculate the increase
     incTemp = profits[i]-profits[i-1]
+    # Add the difference to the increase values
     incprofits.append(incTemp)
+
+    # Assess whether this increase is the highest increase among the dataset
     if(incTemp > greatestIncrease):
         greatestIncrease = incTemp
         greatestIncMonth = months[i]
+
+    # Assess whether this increase is the highest decrease among the dataset
     if(incTemp < greatestDecrease):
         greatestDecrease = incTemp
         greatestDecMonth = months[i]
-    i += 1
 
+# Calculate the totals of the months and their profit of the differences
 avgChange = statistics.mean(incprofits)
 totalValue = sum(profits)
 noMonths = len(months)
 
+# Print results with appropiate format
 print("Financial Analysis")
 print("----------------------------------------------")
 print(f"Total months:  {noMonths}")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,60 +1,91 @@
 # ===============================================
 # Paolo Vega
-# 03/02/2020
-# Version 1.0
-#   Version 1.1 03/04/2020
+# Bootcamp Data Analytics
+# Version   1.0.0 03/02/2020
+#           1.0.1 03/04/2020
+#           1.0.2 03/05/2020
 # File to run the PyPoll analysis
 # ===============================================
 
-# Add required modules to facilitate the implementation
+# Add required modules
 import os
 import csv
 import datetime
 import statistics
 
-folder =  "C:\\Users\\Paolo\\Bootcamp\\Python-Challenge\\Resources"
+# Define the folder and file name location
+folder =  "../Resources"
 csvName = "election_data.csv"
+# Define the output file name
+txtName = "results.txt"
 csvpath = os.path.join(folder,csvName)
 
+# Lists that will store the candidates, their votes and the percentages for each
 candidates = []
-results = []
+votes = []
 percentages = []
+# Total number of votes in the dataset
 totalVotes = 0
-noVotes = 0
 
+# Open and read the csv file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    # Store the header to avoid column names
     header = next(csvreader)
 
-
+    # Go through each line to count the votes and get the candidates names
     for row in csvreader:
         totalVotes += 1
+        # Try to get see if the candidate is already in the list to count his votes,
+        # otherwise we need add it first and start from 0
         try:
-            results[candidates.index(row[2])] += 1
+            votes[candidates.index(row[2])] += 1
         except ValueError:
             candidates.append(row[2])
-            results.append(0)    
+            votes.append(0)    
 
+# Calculate the percentage of votes for each candidate
 for candidate in candidates:
-    percentages.append( (results[candidates.index(candidate)] / totalVotes)*100 )
+    percentages.append( (votes[candidates.index(candidate)] / totalVotes)*100 )
 
+# Print the results
 print("Election Results")
 print("----------------------------------------------")
 print(f"Total Votes:  {totalVotes:,}")
 print("----------------------------------------------")
-
+# Determine who is the winner according to the highest percentage of votes
 winner = 0
 for candidate in candidates:
+    # Identify the candidate based on an index
     index = candidates.index(candidate)
-    print(f"{candidates[index]}: , {percentages[index]:.1f}% ({results[index]:,})")
+    # Print the results with format of number (separate with commas and decimal point)
+    print(f"{candidates[index]}: {percentages[index]:.1f}% ({votes[index]:,})")
+    # Assess whether we are identifying the winner
     if (percentages[index] > percentages[winner]):
         winner = index
-
+# Print the winner among the candidates
 print("----------------------------------------------")
 print(f"Winner:  {candidates[winner]}")
 print("----------------------------------------------")
 
-# Create text file
+# Create text file to write and open it for writing purposes
+resultFile =  open(os.path.join(folder,txtName), "w")
+# Same format as in the terminal
+resultFile.writelines("Election votes\n")
+resultFile.writelines("----------------------------------------------\n")
+resultFile.writelines(f"Total Votes:  {totalVotes:,} \n")
+resultFile.writelines("----------------------------------------------\n")
+# Print the information for each candidate
+for candidate in candidates:
+    # Get the position for each candidate and print votes and percentages with appropiate format
+    index = candidates.index(candidate)
+    resultFile.writelines(f"{candidates[index]}: {percentages[index]:.1f}% ({votes[index]:,}) \n")
+resultFile.writelines("----------------------------------------------\n")
+resultFile.writelines(f"Winner:  {candidates[winner]} \n")
+resultFile.writelines("----------------------------------------------\n")
+# Close the file
+resultFile.close()
+
 
 
 
